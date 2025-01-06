0ce8a333d88f54bdb4277bae320f69b83593d67a
mah098 <mahendra@greatlearning.in>
2025-01-06 08:41:02 +0530

    first commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,19 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Header from "./Header";
+import { validateForm } from "../utils/validate";
 
 const Login = () => {
   const [isSigninForm, setSigninForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const handleButtonClick = () => {
+    //validate data
+    const errText = validateForm(email.current.value, password.current.value);
+    setErrorMessage(errText);
+    console.log(errText);
+  };
   const toggleSigninForm = () => {
     setSigninForm(!isSigninForm);
   };
@@ -17,30 +28,44 @@ const Login = () => {
         />
       </div>
       <div className="absolute h-screen bg-black/30 top-0 w-[100%]"></div>
-      <form className="w-3/12 relative bg-black/85  rounded-md p-6 m-auto mt-36">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+        className="w-3/12 relative bg-black/85  rounded-md p-6 m-auto mt-36"
+      >
         <h1 className="text-white text-2xl my-4">
           {isSigninForm ? "Sign In" : "Sign Up"}
         </h1>
         {!isSigninForm && (
           <input
+            ref={fullname}
             type="text"
             placeholder="Name"
-            className="p-2 my-4 rounded-[4px] border border-white w-[100%] bg-transparent text-sm"
+            className="p-2 my-4 rounded-[4px] border border-white w-[100%] bg-transparent text-sm text-white"
           />
         )}
         <input
+          ref={email}
           type="text"
           placeholder="Email or mobile number"
-          className="p-2 my-4 rounded-[4px] border border-white w-[100%] bg-transparent text-sm"
+          className="p-2 my-4 rounded-[4px] border border-white w-[100%] bg-transparent text-sm text-white"
         />
         <input
+          onFocus={setErrorMessage(" ")}
+          ref={password}
           type="password"
           placeholder="Password"
-          className="p-2 rounded-[4px] my-4 border border-white w-[100%] bg-transparent text-sm"
+          className="p-2 rounded-[4px] my-4 border border-white w-[100%] bg-transparent text-sm text-white"
         />
-        <button className="rounded-[4px] bg-red-600 px-4 py-2 my-4  text-white block w-[100%]">
+        <p className="text-red-400 text-sm py-3">{errorMessage}</p>
+        <button
+          className="rounded-[4px] bg-red-600 px-4 py-2 my-4  text-white block w-[100%]"
+          onClick={handleButtonClick}
+        >
           {isSigninForm ? "Sign In" : "Sign Up"}
         </button>
+
         <p className="text-center text-white text-sm">Forgot password?</p>
         <p
           className="text-center text-white text-sm py-3 cursor-pointer"
